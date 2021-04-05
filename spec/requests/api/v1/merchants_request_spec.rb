fe3eef62f695b597cff699bbe439914cd9cb99ec
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-05 13:21:21 -0500

    Add test for single merchant endpoint.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,14 @@ RSpec.describe "Merchants API" do
     merchants = JSON.parse(response.body, symbolize_names: true)
     expect(merchants).to be_empty
   end
+  it "returns one merchant by the id" do
+    id = create(:merchant).id
+    get "/api/v1/merchants/#{id}"
+    merchant = JSON.parse(response.body, symbolize_names: true)
+    expect(response).to be_successful
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
 end
