0ba98e81c2addff677b76dfe5f75a6721d8a869f
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-05 12:27:34 -0500

    Add attribute tests to merchant index request spec.

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe "Merchants API" do
     create_list(:merchant, 20)
     get '/api/v1/merchants'
     expect(response).to be_successful
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants.count).to eq(20)
+    merchants.each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
   end
 end
