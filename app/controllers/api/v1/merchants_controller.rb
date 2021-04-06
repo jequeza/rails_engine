fb0aaa46ed99aa332a52ed83be36210db5295788
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-06 13:14:33 -0500

    Modify merchant serializer accordingly.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
     merchants = Merchant.all
-    render json: MerchantsSerializer.format_merchants(merchants)
+    render json: MerchantSerializer.new(merchants)
   end
 
   def show
-    merchant = [Merchant.find(params[:id])]
-    render json: MerchantsSerializer.format_merchants(merchant)
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
   end
 end
