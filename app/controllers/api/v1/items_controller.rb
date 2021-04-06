871529a45b77e9ab46bca79c31db06b5d0ce0e7d
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-06 14:44:19 -0500

    Update item serializer and controller.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     items = Item.all
-    render json: ItemsSerializer.format_items(items)
+    render json: ItemSerializer.new(items)
   end
 
   def show
-    item = [Item.find(params[:id])]
-    render json: ItemsSerializer.format_items(item)
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item)
   end
 end
