50c608219736e9693e6948eaa98203d0b845e507
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-06 15:29:05 -0500

    Add functionality to update an item with sent in params.

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -14,6 +14,12 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(item)
   end
 
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    render json: ItemSerializer.new(item)
+  end
+
   private
     def item_params
       params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
