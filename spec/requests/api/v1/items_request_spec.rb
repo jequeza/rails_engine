98e79ef784d7f0ac14450cb47abeca1d4b78e7af
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-06 15:03:44 -0500

    Add test for endpoint to create an Item, sending in params.

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -44,4 +44,22 @@ RSpec.describe "Items API" do
     expect(item[:data][:attributes]).to have_key(:unit_price)
     expect(item[:data][:attributes][:unit_price]).to be_a(Float)
   end
+  it "can create a new item with sent params" do
+    merchant = create(:merchant)
+    item_params = ({
+                    name: "value1",
+                    description: "value2",
+                    unit_price: 100.99,
+                    merchant_id: merchant.id
+                  })
+    headers = {"CONTENT_TYPE" => "application/json"}
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
+
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(merchant.id.to_s)
+  end
 end
