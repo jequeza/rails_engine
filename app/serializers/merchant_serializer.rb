1edf7e4fe060ab359d05d4c4f48983266cd42932
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-06 14:45:01 -0500

    Remove white space.

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,15 +1,4 @@
 class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
-  # def self.format_merchants(merchants)
-  # { data:
-  #   merchants.map do |merchant|
-  #     {
-  #       id: merchant.id,
-  #       type: "merchant",
-  #       attributes: { name: merchant.name }
-  #     }
-  #   end
-  # }
-  # end
 end
