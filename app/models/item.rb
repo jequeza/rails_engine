319dbf7637cf13a74fc5788ea6019fde225708e1
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-07 16:38:45 -0500

    Add method to model, tests pass.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :merchant
   has_many :invoice_items
+
+  def self.find_by_name(name)
+    where("name ilike ?", "%#{name}%").order(name: :asc).limit(1)
+  end
 end
