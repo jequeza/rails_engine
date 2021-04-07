7da8b5a64389b387fb212d5cd69e199d51a712cf
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-06 22:35:59 -0500

    Update index action, add constant for default amount per page, all test pass.

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::MerchantsController < ApplicationController
+  MERCHANTS_PER_PAGE = 20
   def index
-    merchants = Merchant.all
-    render json: MerchantSerializer.new(merchants)
+    if params[:per_page]
+      per_page = params[:per_page].to_i
+      page = params.fetch(:page, 0).to_i
+      merchants = Merchant.offset(page * per_page).limit(per_page)
+      render json: MerchantSerializer.new(merchants)
+    else
+      page = params.fetch(:page, 0).to_i
+      merchants = Merchant.offset(page * MERCHANTS_PER_PAGE).limit(MERCHANTS_PER_PAGE)
+      render json: MerchantSerializer.new(merchants)
+    end
   end
 
   def show
