434b978fd9b670d155809c8ee54533075a74e803
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-07 16:38:17 -0500

    Add test for ::find_by_name of Item model.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,5 +5,19 @@ RSpec.describe Item, type: :model do
     it { should belong_to :merchant }
     it { should have_many :invoice_items }
   end
+
+  describe "class methods" do
+    describe "::find_by_name" do
+      it "returns a record that contains a given name and is sorted alphabetically" do
+        merchant = create(:merchant)
+        gold_ring = create(:item, merchant_id: merchant.id, name: "Gold Ring" )
+        silver_ring = create(:item, merchant_id: merchant.id, name: "Awesome Silver Ring")
+        diamond_ring = create(:item, merchant_id: merchant.id, name: "Diamond Ring")
+
+        name = "ring"
+        expect(Item.find_by_name(name)[0]).to eq(silver_ring)
+      end
+    end
+  end
 end
 
