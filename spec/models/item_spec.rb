81ba48de2a5a49cff5ab4a997a56d152f55cfd8b
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-08 11:48:30 -0500

    Add test for class method find_by_price of Item.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,9 +13,19 @@ RSpec.describe Item, type: :model do
         gold_ring = create(:item, merchant_id: merchant.id, name: "Gold Ring" )
         silver_ring = create(:item, merchant_id: merchant.id, name: "Awesome Silver Ring")
         diamond_ring = create(:item, merchant_id: merchant.id, name: "Diamond Ring")
-
         name = "ring"
         expect(Item.find_by_name(name)[0]).to eq(silver_ring)
+      end
+      it "the search handles case sensitive searches" do
+        merchant = create(:merchant)
+        gold_ring = create(:item, merchant_id: merchant.id, name: "Gold Ring" )
+        silver_ring = create(:item, merchant_id: merchant.id, name: "Awesome Silver ring")
+        diamond_ring = create(:item, merchant_id: merchant.id, name: "Diamond RING")
+        name = "riNG"
+        expect(Item.find_by_name(name).length).to eq(3)
+        expect(Item.find_by_name(name).first).to eq(silver_ring)
+        expect(Item.find_by_name(name).second).to eq(diamond_ring)
+        expect(Item.find_by_name(name).third).to eq(gold_ring)
       end
     end
   end
