d3083ebf303cb9bea263004f621004863d468a74
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-08 11:48:50 -0500

    Add method and implement query.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,10 @@ class Item < ApplicationRecord
   has_many :invoice_items
 
   def self.find_by_name(name)
-    where("name ilike ?", "%#{name}%").order(name: :asc).limit(1)
+    where("name ilike ?", "%#{name}%").order(name: :asc)
+  end
+  def self.find_by_price(price)
+    format_price = price.to_f
+    where("unit_price >= ?", format_price).order(unit_price: :asc)
   end
 end
