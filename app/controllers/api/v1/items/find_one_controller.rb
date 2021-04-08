38bfbb6390392b3f629b8134b289bf4ad250b667
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-08 14:27:35 -0500

    Update controller show action logic.

--- app/controllers/api/v1/items/find_one_controller.rb
+++ app/controllers/api/v1/items/find_one_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::Items::FindOneController < ApplicationController
   def show
-    if params[:name]
-      found_item = Item.find_by_name(params[:name])
+    if params[:min_price] && params[:max_price]
+      found_item = Item.find_by_price_range(params[:min_price], params[:max_price])
       render json: ItemSerializer.new(found_item[0])
     elsif params[:min_price]
-      found_item = Item.find_by_price(params[:min_price])
+      found_item = Item.find_by_min_price(params[:min_price])
+      render json: ItemSerializer.new(found_item[0])
+    elsif params[:max_price]
+      found_item = Item.find_by_max_price(params[:max_price])
+      render json: ItemSerializer.new(found_item[0])
+    elsif params[:name]
+      found_item = Item.find_by_name(params[:name])
       render json: ItemSerializer.new(found_item[0])
     end
   end
