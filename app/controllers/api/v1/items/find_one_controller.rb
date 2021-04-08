b475d19a2d45c21bf74e0046b1adc74133dd3767
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-08 11:49:43 -0500

    Update controller action, requests test is passing.

--- app/controllers/api/v1/items/find_one_controller.rb
+++ app/controllers/api/v1/items/find_one_controller.rb
@@ -3,6 +3,9 @@ class Api::V1::Items::FindOneController < ApplicationController
     if params[:name]
       found_item = Item.find_by_name(params[:name])
       render json: ItemSerializer.new(found_item[0])
+    elsif params[:min_price]
+      found_item = Item.find_by_price(params[:min_price])
+      render json: ItemSerializer.new(found_item[0])
     end
   end
 end
