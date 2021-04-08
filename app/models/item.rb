cba0decf50d593cdb44d50e3d88719084955e2ce
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-08 14:27:07 -0500

    Add methods to Item model, all tests pass.

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,22 @@ class Item < ApplicationRecord
   def self.find_by_name(name)
     where("name ilike ?", "%#{name}%").order(name: :asc)
   end
-  def self.find_by_price(price)
+
+  def self.find_by_min_price(price)
     format_price = price.to_f
-    where("unit_price >= ?", format_price).order(unit_price: :asc)
+    where("unit_price >= ?", format_price).order(name: :asc)
+  end
+
+  def self.find_by_max_price(price)
+    format_price = price.to_f
+    where("unit_price <= ?", format_price).order(name: :asc)
+  end
+
+  def self.find_by_price_range(min_price, max_price)
+    format_max_price = max_price.to_f
+    format_min_price = min_price.to_f
+    where("unit_price >= ? and unit_price <= ?", "#{format_min_price}", "#{format_max_price}")
+    .distinct
+    .order(name: :asc)
   end
 end
