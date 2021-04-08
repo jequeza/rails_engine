ab29eca1dcd9e25070b97bd865745192660664fe
Jesus Quezada-Guillen <maj3st1@gmail.com>
2021-04-08 14:25:45 -0500

    Add tests for class methods to search for items by price range.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -28,6 +28,55 @@ RSpec.describe Item, type: :model do
         expect(Item.find_by_name(name).third).to eq(gold_ring)
       end
     end
+    describe "::find_by_min_price" do
+      it "returns an item list where the unit_price is more than min price given and sorted alphabetically" do
+        merchant = create(:merchant)
+        gold_ring = create(:item, merchant_id: merchant.id, name: "Gold Ring", unit_price: 749.00)
+        silver_ring = create(:item, merchant_id: merchant.id, name: "Silver Ring", unit_price: 200.99)
+        mood_ring = create(:item, merchant_id: merchant.id, name: "Mood Ring", unit_price: 49.99)
+        diamond_necklace = create(:item, merchant_id: merchant.id, name: "Diamond Necklace", unit_price: 2100.99)
+        min_price = 300.0
+
+        expect(Item.find_by_min_price(min_price).first).to eq(diamond_necklace)
+        expect(Item.find_by_min_price(min_price).length).to eq(2)
+        expect(Item.find_by_min_price(min_price).first.unit_price).to be > min_price
+        expect(Item.find_by_min_price(min_price).second.unit_price).to be > min_price
+      end
+    end
+    describe "::find_by_max_price" do
+      it "returns an item list where the unit_price is less than max price given and sorted alphabetically" do
+        merchant = create(:merchant)
+        gold_ring = create(:item, merchant_id: merchant.id, name: "Gold Ring", unit_price: 749.00)
+        silver_ring = create(:item, merchant_id: merchant.id, name: "Silver Ring", unit_price: 200.99)
+        mood_ring = create(:item, merchant_id: merchant.id, name: "Mood Ring", unit_price: 49.99)
+        diamond_necklace = create(:item, merchant_id: merchant.id, name: "Diamond Necklace", unit_price: 2100.99)
+        max_price = 400.0
+
+        expect(Item.find_by_max_price(max_price).first).to eq(mood_ring)
+        expect(Item.find_by_max_price(max_price).length).to eq(2)
+        expect(Item.find_by_max_price(max_price).first.unit_price).to be < max_price
+        expect(Item.find_by_max_price(max_price).second.unit_price).to be < max_price
+      end
+    end
+    describe "::find_by_price_range" do
+      it "returns an item list where the unit_price is in range between min and max price given and sorted alphabetically" do
+        merchant = create(:merchant)
+        gold_ring = create(:item, merchant_id: merchant.id, name: "Gold Ring", unit_price: 749.00)
+        silver_ring = create(:item, merchant_id: merchant.id, name: "Silver Ring", unit_price: 200.99)
+        mood_ring = create(:item, merchant_id: merchant.id, name: "Mood Ring", unit_price: 49.99)
+        diamond_necklace = create(:item, merchant_id: merchant.id, name: "Diamond Necklace", unit_price: 2100.99)
+        golden_chain = create(:item, merchant_id: merchant.id, name: "Awesome Gold Chain", unit_price: 999.99)
+        min_price = 150.00
+        max_price = 1000.00
+        expect(Item.find_by_price_range(min_price, max_price).first).to eq(golden_chain)
+        expect(Item.find_by_price_range(min_price, max_price).length).to eq(3)
+        expect(Item.find_by_price_range(min_price, max_price).first.unit_price).to be < max_price
+        expect(Item.find_by_price_range(min_price, max_price).first.unit_price).to be > min_price
+        expect(Item.find_by_price_range(min_price, max_price).second.unit_price).to be < max_price
+        expect(Item.find_by_price_range(min_price, max_price).second.unit_price).to be > min_price
+        expect(Item.find_by_price_range(min_price, max_price).third.unit_price).to be < max_price
+        expect(Item.find_by_price_range(min_price, max_price).third.unit_price).to be > min_price
+      end
+    end
   end
 end
-
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -132,12 +132,27 @@ RSpec.describe "Items API" do
     expect(response).to be_successful
     item = JSON.parse(response.body, symbolize_names: true)
     expect(item[:data][:attributes][:unit_price]).to be > price
-    expect(item[:data][:attributes][:name]).to eq("Gold Ring")
+    expect(item[:data][:attributes][:name]).to eq("Diamond Necklace")
 
     get "/api/v1/items/find_one?max_price=#{price}"
     expect(response).to be_successful
     item = JSON.parse(response.body, symbolize_names: true)
     expect(item[:data][:attributes][:unit_price]).to be < price
-    expect(item[:data][:attributes][:name]).to eq("Silver Ring")
+    expect(item[:data][:attributes][:name]).to eq("Mood Ring")
+  end
+  it "can find an item with price min and maximun price" do
+    merchant = create(:merchant)
+    gold_ring = create(:item, merchant_id: merchant.id, name: "Gold Ring", unit_price: 749.00)
+    silver_ring = create(:item, merchant_id: merchant.id, name: "Silver Ring", unit_price: 200.99)
+    mood_ring = create(:item, merchant_id: merchant.id, name: "Mood Ring", unit_price: 49.99)
+    diamond_necklace = create(:item, merchant_id: merchant.id, name: "Diamond Necklace", unit_price: 2100.99)
+    maximun_price = 900.00
+    minimun_price = 150.00
+
+    get "/api/v1/items/find_one?max_price=#{maximun_price}&min_price=#{minimun_price}"
+
+    expect(response).to be_successful
+    item = JSON.parse(response.body, symbolize_names: true)
+    expect(item[:data][:attributes][:name]).to eq(gold_ring.name)
   end
 end
